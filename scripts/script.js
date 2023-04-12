2d7447f90d2739637254568f59cae49d38caf55a
Peally <pieclandre@gmail.com>
2023-04-12 20:20:05 +0200

    feat : filter all tag and display them

--- scripts/script.js
+++ scripts/script.js
@@ -43,3 +43,39 @@ for (let i = 0; i < recipes.length; i++) {
 
   recipesSection.appendChild(recipesTemplate);
 }
+
+const tagContainers = document.querySelectorAll(".container__tag");
+
+tagContainers.forEach((tagContainer) => {
+  let currentTag = tagContainer.getAttribute("data-tag");
+  let tags = [];
+
+  if (currentTag === "ingredients") {
+    recipes.forEach((recipe) => {
+      for (let i = 0; i < recipe.ingredients.length; i++) {
+        if (!tags.includes(recipe.ingredients[i].ingredient)) {
+          tags.push(recipe.ingredients[i].ingredient);
+        }
+      }
+    });
+  } else if (currentTag === "ustensils") {
+    recipes.forEach((recipe) => {
+      for (let i = 0; i < recipe.ustensils.length; i++) {
+        if (!tags.includes(recipe.ustensils[i])) {
+          tags.push(recipe.ustensils[i]);
+        }
+      }
+    });
+  } else if (currentTag === "appliance") {
+    recipes.forEach((recipe) => {
+      tags.push(recipe.appliance);
+    });
+  }
+  tags.forEach((tag) => {
+    const tagDOMSpan = document.createElement("span");
+    tagDOMSpan.innerText = tag;
+    tagContainer.appendChild(tagDOMSpan);
+  });
+});
+
+const filterTag = () => {};
