cce799757d2cfdd24320ec13271f8a01b8acd018
Peally <pieclandre@gmail.com>
2023-04-14 17:19:03 +0200

    Refacto code to display recipe and handle search input

---
+++ scripts/DOM/tags.js
@@ -0,0 +1,58 @@
+const handleTags = (recipes) => {
+  const tagContainers = document.querySelectorAll(".container__tag");
+
+  tagContainers.forEach((tagContainer) => {
+    let currentTag = tagContainer.getAttribute("data-tag");
+    let tags;
+
+    switch (currentTag) {
+      case "ingredients":
+        tags = captureTags(currentTag, recipes);
+        break;
+      case "appliance":
+        tags = captureTags(currentTag, recipes);
+        break;
+      case "ustensils":
+        tags = captureTags(currentTag, recipes);
+        break;
+      default:
+        break;
+    }
+
+    tags.forEach((tag) => {
+      const tagDOMli = document.createElement("li");
+      tagDOMli.innerText = tag;
+      tagContainer.appendChild(tagDOMli);
+    });
+  });
+};
+
+const captureTags = (option, recipes) => {
+  let targets;
+  const tags = [];
+  recipes.forEach((recipe) => {
+    if (option === "ingredients") {
+      targets = recipe.ingredients;
+    } else if (option === "ustensils") {
+      targets = recipe.ustensils;
+    } else if (option === "appliance") {
+      targets = recipe.appliance;
+    }
+
+    if (Array.isArray(targets)) {
+      targets.forEach((target) => {
+        if (!tags.includes(target.ingredient ? target.ingredient : target)) {
+          tags.push(target.ingredient ? target.ingredient : target);
+        }
+      });
+    } else {
+      if (!tags.includes(targets)) {
+        tags.push(targets);
+      }
+    }
+  });
+
+  return tags;
+};
+
+export { handleTags };
