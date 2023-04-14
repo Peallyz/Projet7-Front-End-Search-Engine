3ad0d9ebd0fc1221e52ccbbf10ba5539a8cec3f9
Peally <pieclandre@gmail.com>
2023-04-14 17:19:15 +0200

    Refacto code to display recipe and handle search input

---
+++ scripts/DOM/recipes.js
@@ -0,0 +1,50 @@
+const createRecipeDOMElements = (recipes) => {
+  const allRecipesDOMElement = [];
+
+  recipes.forEach((recipe) => {
+    const recipesTemplate = document.createElement("article");
+    recipesTemplate.innerHTML = `<article>
+      <div class="img"></div>
+      <div class="content">
+        <div class="content__main">
+          <h2>${recipe.name}</h2>
+          <div class="time">
+            <img
+              src="./assets/clock.svg"
+              alt="Horloge indiquant le temps de préparation du plat"
+            />
+            <p>${recipe.time} min</p>
+          </div>
+        </div>
+        <div class="content__sub">
+          <div class="ingredient">
+	    	<ul>
+      	${captureIngredient(recipe)}
+	    	</ul>
+          </div>
+          <div class="details">
+            <p>${recipe.description}</p>
+          </div>
+        </div>
+      </div>
+    </article>
+    `;
+    allRecipesDOMElement.push(recipesTemplate);
+  });
+  return allRecipesDOMElement;
+};
+
+const captureIngredient = (recipe) => {
+  let ingredientsList = "";
+
+  recipe.ingredients.forEach((el) => {
+    const ingredient = `<li>${el.ingredient}${
+      el.quantity ? ": " + el.quantity : ""
+    } ${el.unit ? el.unit : ""}</li>`;
+    ingredientsList += ingredient;
+  });
+
+  return ingredientsList;
+};
+
+export { createRecipeDOMElements };
