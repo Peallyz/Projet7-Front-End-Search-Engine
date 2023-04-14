1c07cf3352ef9316c672fa4f0212a56378d224c3
Peally <pieclandre@gmail.com>
2023-04-14 17:18:57 +0200

    Refacto code to display recipe and handle search input

--- scripts/script.js
+++ scripts/script.js
@@ -1,95 +1,18 @@
+import { createRecipeDOMElements } from "./DOM/recipes.js";
+import { handleTags } from "./DOM/tags.js";
 import recipes from "./data/recipes.js";
+import { launchEventOnTagSearchInput } from "./utils/eventListener.js";
 
-const recipesSection = document.querySelector(".recipes");
+const displayRecipes = (recipes) => {
+  const recipesSection = document.querySelector(".recipes");
+  const currentRecipe = createRecipeDOMElements(recipes);
+  currentRecipe.forEach((recipe) => recipesSection.appendChild(recipe));
+};
 
-for (let i = 0; i < recipes.length; i++) {
-  let ingredientsList = "";
-  for (let j = 0; j < recipes[i].ingredients.length; j++) {
-    const ingredient = `<li>${recipes[i].ingredients[j].ingredient}${
-      recipes[i].ingredients[j].quantity
-        ? ": " + recipes[i].ingredients[j].quantity
-        : ""
-    } ${
-      recipes[i].ingredients[j].unit ? recipes[i].ingredients[j].unit : ""
-    }</li>`;
+const init = (recipes) => {
+  displayRecipes(recipes);
+  handleTags(recipes);
+  launchEventOnTagSearchInput();
+};
 
-    ingredientsList += ingredient;
-  }
-  const recipesTemplate = document.createElement("article");
-  recipesTemplate.innerHTML = `<article>
-      <div class="img"></div>
-      <div class="content">
-        <div class="content__main">
-          <h2>${recipes[i].name}</h2>
-          <div class="time">
-            <img
-              src="./assets/clock.svg"
-              alt="Horloge indiquant le temps de préparation du plat"
-            />
-            <p>${recipes[i].time} min</p>
-          </div>
-        </div>
-        <div class="content__sub">
-          <div class="ingredient">
-            ${ingredientsList}
-          </div>
-          <div class="details">
-            <p>${recipes[i].description}</p>
-          </div>
-        </div>
-      </div>
-    </article>
-    `;
-
-  recipesSection.appendChild(recipesTemplate);
-}
-
-const tagContainers = document.querySelectorAll(".container__tag");
-
-tagContainers.forEach((tagContainer) => {
-  let currentTag = tagContainer.getAttribute("data-tag");
-  let tags = [];
-
-  if (currentTag === "ingredients") {
-    recipes.forEach((recipe) => {
-      for (let i = 0; i < recipe.ingredients.length; i++) {
-        if (!tags.includes(recipe.ingredients[i].ingredient)) {
-          tags.push(recipe.ingredients[i].ingredient);
-        }
-      }
-    });
-  } else if (currentTag === "ustensils") {
-    recipes.forEach((recipe) => {
-      for (let i = 0; i < recipe.ustensils.length; i++) {
-        if (!tags.includes(recipe.ustensils[i])) {
-          tags.push(recipe.ustensils[i]);
-        }
-      }
-    });
-  } else if (currentTag === "appliance") {
-    recipes.forEach((recipe) => {
-      tags.push(recipe.appliance);
-    });
-  }
-  tags.forEach((tag) => {
-    const tagDOMli = document.createElement("li");
-    tagDOMli.innerText = tag;
-    tagContainer.appendChild(tagDOMli);
-  });
-});
-
-const buttons = document.querySelectorAll("button");
-const chevrons = document.querySelectorAll(".filter__byTag span");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.parentNode.classList.add("extended");
-    button.parentNode.classList.remove("reduced");
-  });
-});
-chevrons.forEach((chevron) => {
-  chevron.addEventListener("click", () => {
-    chevron.parentNode.classList.toggle("extended");
-    chevron.parentNode.classList.toggle("reduced");
-  });
-});
+init(recipes);
