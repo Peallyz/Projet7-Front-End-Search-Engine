47375158fbc3192f23448e06b6f122c813723c36
Peally <pieclandre@gmail.com>
2023-04-15 18:24:35 +0200

    feat : filtered list main input using array methode

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ import { launchEventOnTagSearchInput } from "./utils/eventListener.js";
 
 const displayRecipes = (recipes) => {
   const recipesSection = document.querySelector(".recipes");
+  recipesSection.innerHTML = "";
   const currentRecipe = createRecipeDOMElements(recipes);
   currentRecipe.forEach((recipe) => recipesSection.appendChild(recipe));
 };
@@ -19,6 +20,48 @@ mainSearchInput.addEventListener("input", (e) => {
     handleTags(recipes);
   }
 });
+
+const updateAvailableRecipe = (filter, recipes) => {
+  let filteredList = [];
+
+  recipes.map((recipe) => {
+    if (
+      recipe.name.toLowerCase().trim().includes(filter.toLowerCase().trim()) ||
+      recipe.description
+        .toLowerCase()
+        .trim()
+        .includes(filter.toLowerCase().trim()) ||
+      recipe.appliance
+        .toLowerCase()
+        .trim()
+        .includes(filter.toLowerCase().trim())
+    ) {
+      if (!filteredList.includes(recipe)) filteredList.push(recipe);
+    }
+
+    recipe.ustensils.map((ustensil) => {
+      if (ustensil.toLowerCase().trim().includes(filter.toLowerCase().trim())) {
+        if (!filteredList.includes(recipe)) filteredList.push(recipe);
+      }
+    });
+
+    recipe.ingredients.map((ingredient) => {
+      if (
+        ingredient.ingredient
+          .toLowerCase()
+          .trim()
+          .includes(filter.toLowerCase().trim())
+      ) {
+        if (!filteredList.includes(recipe)) filteredList.push(recipe);
+      }
+    });
+  });
+
+  displayRecipes(filteredList);
+  handleTags(filteredList);
+
+  return filteredList;
+};
 
 const init = (recipes) => {
   displayRecipes(recipes);
