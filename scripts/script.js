3ee7446fb67b9f2bb49998a63b4b1ec3ab177d8f
Peally <pieclandre@gmail.com>
2023-04-15 18:16:16 +0200

    feat : updateAvailableRecipe with native loop

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ import { launchEventOnTagSearchInput } from "./utils/eventListener.js";
 
 const displayRecipes = (recipes) => {
   const recipesSection = document.querySelector(".recipes");
+  recipesSection.innerHTML = "";
   const currentRecipe = createRecipeDOMElements(recipes);
   currentRecipe.forEach((recipe) => recipesSection.appendChild(recipe));
 };
@@ -19,6 +20,46 @@ mainSearchInput.addEventListener("input", (e) => {
     handleTags(recipes);
   }
 });
+
+let updateAvailableRecipe = (filter, recipes) => {
+  let filteredList = [];
+
+  for (let recipe of recipes) {
+    if (
+      recipe.name.toLowerCase().trim().indexOf(filter.toLowerCase().trim()) >
+        -1 ||
+      recipe.description
+        .toLowerCase()
+        .trim()
+        .indexOf(filter.toLowerCase().trim()) > -1 ||
+      recipe.appliance
+        .toLowerCase()
+        .trim()
+        .indexOf(filter.toLowerCase().trim()) > -1
+    ) {
+      filteredList.push(recipe);
+
+      continue;
+    }
+
+    for (let ustensil of recipe.ustensils) {
+      if (ustensil.toLowerCase().trim().indexOf(filter) > -1) {
+        filteredList.push(recipe);
+        break;
+      }
+    }
+
+    for (let ingredient of recipe.ingredients) {
+      if (ingredient.ingredient.toLowerCase().trim().indexOf(filter) > -1) {
+        filteredList.push(recipe);
+        break;
+      }
+    }
+  }
+  displayRecipes(filteredList);
+  handleTags(filteredList);
+  return filteredList;
+};
 
 const init = (recipes) => {
   displayRecipes(recipes);
