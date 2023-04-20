5f3b00f313f8229ff5f77201faba0fe06df6e4e4
Peally <pieclandre@gmail.com>
2023-04-20 18:40:47 +0200

    feat : create updateAvailableRecipeWithInput Function and updateAvailableRecipeWithTag

--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -27,4 +27,85 @@ const updateTags = (e) => {
     list.forEach((element) => (element.style.display = "block"));
   }
 };
-export { toggleSearchInput, updateTags };
+
+const updateAvailableRecipeWithInput = (recipes, filter, filteredList) => {
+  recipes.map((recipe) => {
+    if (
+      recipe.name.toLowerCase().trim().includes(filter.toLowerCase().trim()) ||
+      recipe.description
+        .toLowerCase()
+        .trim()
+        .includes(filter.toLowerCase().trim()) ||
+      recipe.appliance
+        .toLowerCase()
+        .trim()
+        .includes(filter.toLowerCase().trim())
+    ) {
+      if (!filteredList.includes(recipe)) filteredList.push(recipe);
+    }
+
+    if (!filteredList.includes(recipe)) {
+      recipe.ustensils.map((ustensil) => {
+        if (
+          ustensil.toLowerCase().trim().includes(filter.toLowerCase().trim())
+        ) {
+          filteredList.push(recipe);
+        }
+      });
+      if (!filteredList.includes(recipe)) {
+        recipe.ingredients.map((ingredient) => {
+          if (
+            ingredient.ingredient
+              .toLowerCase()
+              .trim()
+              .includes(filter.toLowerCase().trim())
+          ) {
+            filteredList.push(recipe);
+          }
+        });
+      }
+    }
+  });
+  return filteredList;
+};
+const updateAvailableRecipeWithTag = (filteredList, allSelectedTags) => {
+  let cleanFilteredList = [...filteredList];
+  allSelectedTags.forEach((tag) => {
+    if (tag.classList.value.includes("ingredient")) {
+      cleanFilteredList = [...cleanFilteredList].filter((recipe) => {
+        let ingredientsList = "";
+        recipe.ingredients.forEach(
+          (element) => (ingredientsList += element.ingredient)
+        );
+
+        return ingredientsList
+          .toLowerCase()
+          .trim()
+          .includes(tag.innerText.toLowerCase().trim());
+      });
+    } else if (tag.classList.value.includes("device")) {
+      cleanFilteredList = [...cleanFilteredList].filter(
+        (recipe) =>
+          recipe.appliance.toLowerCase().trim() ===
+          tag.innerText.toLowerCase().trim()
+      );
+    } else if (tag.classList.value.includes("ustensil")) {
+      cleanFilteredList = [...cleanFilteredList].filter((recipe) =>
+        recipe.ustensils
+          .join("")
+          .toLowerCase()
+          .trim()
+          .includes(tag.innerText.toLowerCase().trim())
+      );
+    }
+  });
+  console.log(cleanFilteredList);
+  return cleanFilteredList;
+};
+
+export {
+  toggleSearchInput,
+  updateTags,
+  updateAvailableRecipeWithTag,
+  updateAvailableRecipeWithInput,
+};
