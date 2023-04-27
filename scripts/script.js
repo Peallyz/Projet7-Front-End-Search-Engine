72d2ad2185af5da1892ea3048ff2a1a387f50022
Peally <pieclandre@gmail.com>
2023-04-27 22:18:07 +0200

    refacto : moving code into recipes.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-import { createRecipeDOMElements } from "./DOM/recipes.js";
+import { displayRecipes } from "./DOM/recipes.js";
 import { handleTags } from "./DOM/tags.js";
 import recipes from "./data/recipes.js";
 import {
@@ -10,20 +10,6 @@ import {
   updateAvailableRecipeWithInput,
   updateAvailableRecipeWithTag,
 } from "./utils/utils.js";
-
-const displayRecipes = (recipes) => {
-  const recipesSection = document.querySelector(".recipes");
-  recipesSection.innerHTML = "";
-  if (recipes.length > 0) {
-    const currentRecipe = createRecipeDOMElements(recipes);
-    currentRecipe.forEach((recipe) => recipesSection.appendChild(recipe));
-  } else {
-    const noRecipeMessage = document.createElement("h2");
-    noRecipeMessage.innerText = `Aucune recette ne correspond à votre critère… vous pouvez
-chercher « tarte aux pommes », « poisson », etc.`;
-    recipesSection.appendChild(noRecipeMessage);
-  }
-};
 
 const mainSearchInput = document.querySelector("#main__research");
 
